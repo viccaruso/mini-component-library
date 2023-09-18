c0f79dd8ad97f7fa8586e77bdfae0dfbe9a1a638
Victor Caruso <vic.caruso@gmail.com>
2023-09-18 15:50:28 -0700

    Add dynamic styles based on size and use css variables to apply

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,40 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+  },
+  large: { fontSize: 18, iconSize: 24, borderThickness: 2, height: 36 },
+};
+
 const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(
+      `Invalid size for IconInput. Options are: "small" or "large". You provided ${size}`
+    );
+  }
+
   return (
     <Wrapper>
       <VisuallyHidden>{label}</VisuallyHidden>
-      <IconWrapper style={{ '--size': 16 + 'px' }}>
-        <Icon id={icon} size={16} />
+      <IconWrapper style={{ '--size': styles.iconSize + 'px' }}>
+        <Icon id={icon} size={styles.iconSize} />
       </IconWrapper>
-      <TextInput {...delegated} />
+      <TextInput
+        {...delegated}
+        style={{
+          '--width': width + 'px',
+          '--height': styles.height / 16 + 'rem',
+          '--border-thickness': styles.borderThickness + 'px',
+          '--font-size': styles.fontSize / 16 + 'rem',
+        }}
+      />
     </Wrapper>
   );
 };
@@ -22,6 +48,11 @@ const Wrapper = styled.label`
    {
     display: block;
     position: relative;
+    color: ${COLORS.gray700};
+
+    &:hover {
+      color: ${COLORS.black};
+    }
   }
 `;
 
@@ -37,10 +68,20 @@ const IconWrapper = styled.div`
 
 const TextInput = styled.input`
    {
-    height: ${24 / 16}rem;
+    height: var(--height);
     border: none;
-    border-bottom: 1px solid ${COLORS.black};
-    padding-left: 24px;
+    border-bottom: var(--border-thickness) solid ${COLORS.black};
+    padding-left: var(--height);
+    color: inherit;
+    font-size: var(--font-size);
+    font-weight: 700;
+    outline-offset: 2px;
+    width: var(--width);
+
+    &::placeholder {
+      color: ${COLORS.gray500};
+      font-weight: 400;
+    }
   }
 `;
 
