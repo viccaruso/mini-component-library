c7ba0422d011b1285d6763c08c8aa45996f945af
Victor Caruso <vic.caruso@gmail.com>
2023-09-18 15:08:00 -0700

    Render unstyled icon and text input on screen

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,20 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Icon id={icon} />
+      <input />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.label`
+   {
+    display: block;
+  }
+`;
 
 export default IconInput;
